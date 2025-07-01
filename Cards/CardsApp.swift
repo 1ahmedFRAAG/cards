e8c97a6fade5db0f8f0e67fd7b87c0caca633744
ahmed <ahmedfraag255@gmail.com>
2025-07-02 00:53:23 +0200

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by AHMED on 02/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
